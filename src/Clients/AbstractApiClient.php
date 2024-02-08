8d1e2f00d358e7a0bee66ad12a17fdfe00cd47fb
Vlad Albescu <71697888+vlad-agrocity@users.noreply.github.com>
2024-02-08 13:49:40 +0200

    Stopped uppercase method name from propagating

--- src/Clients/AbstractApiClient.php
+++ src/Clients/AbstractApiClient.php
@@ -68,8 +68,8 @@ abstract class AbstractApiClient
         if (!$endpointUrl)
             throw new BadApiEndpointConfig("Bad url in api endpoint config for provider {$this->providerName}, endpoint {$endpointName}");
 
-        $endpointMethod = $endpointConfig['method'] ?? 'get';
-        if (!in_array(strtolower($endpointMethod), ['get', 'post', 'patch', 'put', 'head', 'delete']))
+        $endpointMethod = strtolower($endpointConfig['method']) ?? 'get';
+        if (!in_array($endpointMethod, ['get', 'post', 'patch', 'put', 'head', 'delete']))
             throw new BadApiEndpointConfig("Bad method in api endpoint config for provider {$this->providerName}, endpoint {$endpointName}");
 
         return [
